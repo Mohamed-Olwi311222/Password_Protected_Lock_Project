c43a45c0c26e343514a89dd00cd6695b8e4c5bbd
Mohamed Olwi <acemido2004@gmail.com>
2024-10-13 00:51:14 +0200

    Added a buzzer to the project for wrong password inputs and modified the connection of the lcd

--- Project.X/ECU_Layer/ecu_layer_init.c
+++ Project.X/ECU_Layer/ecu_layer_init.c
@@ -32,37 +32,43 @@ keypad_t keypad =   {
 
 /* LCD for showing the message*/
 char_lcd_4bit_t lcd = {
-    .lcd_rs.port = PORTC_INDEX,
-    .lcd_rs.pin = GPIO_PIN7,
+    .lcd_rs.port = PORTD_INDEX,
+    .lcd_rs.pin = GPIO_PIN0,
     .lcd_rs.direction = GPIO_DIRECTION_OUTPUT,
     .lcd_rs.logic = GPIO_LOW,
     
     .lcd_en.port = PORTD_INDEX,
-    .lcd_en.pin = GPIO_PIN0,
+    .lcd_en.pin = GPIO_PIN1,
     .lcd_en.direction = GPIO_DIRECTION_OUTPUT,
     .lcd_en.logic = GPIO_LOW,
     
     .lcd_data[0].port = PORTD_INDEX,
-    .lcd_data[0].pin = GPIO_PIN1,
+    .lcd_data[0].pin = GPIO_PIN2,
     .lcd_data[0].direction = GPIO_DIRECTION_OUTPUT,
     .lcd_data[0].logic = GPIO_LOW,
     
     .lcd_data[1].port = PORTD_INDEX,
-    .lcd_data[1].pin = GPIO_PIN2,
+    .lcd_data[1].pin = GPIO_PIN3,
     .lcd_data[1].direction = GPIO_DIRECTION_OUTPUT,
     .lcd_data[1].logic = GPIO_LOW,
     
     .lcd_data[2].port = PORTD_INDEX,
-    .lcd_data[2].pin = GPIO_PIN3,
+    .lcd_data[2].pin = GPIO_PIN4,
     .lcd_data[2].direction = GPIO_DIRECTION_OUTPUT,
     .lcd_data[2].logic = GPIO_LOW,
     
     .lcd_data[3].port = PORTD_INDEX,
-    .lcd_data[3].pin = GPIO_PIN4,
+    .lcd_data[3].pin = GPIO_PIN5,
     .lcd_data[3].direction = GPIO_DIRECTION_OUTPUT,
     .lcd_data[3].logic = GPIO_LOW
 };
-
+/* A buzzer to ring*/
+pin_config_t buzzer = {
+    .direction = GPIO_DIRECTION_OUTPUT,
+    .logic = GPIO_LOW,
+    .pin = GPIO_PIN6,
+    .port = PORTD_INDEX
+};
 /* DC motor 1 to act as lock 1*/
 dc_motor_t  dc_motor_1 = {
     .dc_motor_pins[DC_MOTOR_PIN1].port = PORTA_INDEX,
@@ -94,6 +100,7 @@ Std_ReturnType ecu_layer_initialize(void)
     Std_ReturnType ret = E_OK;
     ret |= keypad_initialize(&keypad);
     ret |= lcd_4bit_initialize(&lcd);
+    ret |= gpio_pin_initialize(&buzzer);
     ret |= dc_motor_initialize(&dc_motor_1);
     ret |= dc_motor_initialize(&dc_motor_2);
     return (ret);
